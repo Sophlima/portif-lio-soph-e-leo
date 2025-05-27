dd5d29f1012bdce2aabece45abdb1e5d81dac293
Sophlima <142442505+Sophlima@users.noreply.github.com>
2025-05-27 12:24:28 -0300

    Add files via upload

--- exercicio3/script3.js
+++ exercicio3/script3.js
@@ -1,15 +1,19 @@
-var nota1 = Number(window.promt("Digite sua nota: "));
-var nota2 = Number(window.promt("Digite sua nota 2: "));
-var nota3 = Number(window.promt("Digite sua nota 3: "));
+let nome
+let nota1 = Number(window.promt("Digite sua nota: "))
+let nota2 = Number(window.promt("Digite sua nota 2: "))
+let nota3 = Number(window.promt("Digite sua nota 3: "))
+let pontosFaltando;
 
 let total = (nota1 + nota2 + nota3)/3;
 
-alert("media final: " + total);
+alura = "alura"
+
+alert("media final: " + total)
 
 if(total >= 60){
-    alert("Você atingiu a média " + total);
-    alert("Aprovado");
+    alert("Você atingiu a média " + total)
+    alert("Aprovado")
 } else {
-    pontosFaltando = 60 - total;
-    alert("Reprovado, faltatam " + pontosFaltando + " pontos");
+    pontosFaltando = 60 - total
+    alert(nome + "Reprovado, faltatam " + pontosFaltando + " pontos, estuda ai: " + alura) 
 }
