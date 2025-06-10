311229ca9fbbe73e22c6e4aca027ec1be856ebb8
Sophlima <142442505+Sophlima@users.noreply.github.com>
2025-06-10 08:49:14 -0300

    Add files via upload

---
+++ exercicio5/script5.js
@@ -0,0 +1,14 @@
+let num1;
+let num2;
+let resultado;
+
+function somar(){
+num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+num2 = Number(window.prompt("Digite aqui o primeiro número"));  
+}
+
+resultado = num1 + num2;
+
+
+
+
